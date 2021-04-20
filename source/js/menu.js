3c7f235f9110e234e7ff48778c4dd8cb6664d199
olga2102 <70343745+olga2102@users.noreply.github.com>
2021-04-21 00:35:08 +0300

    ретина и доработки

--- source/js/menu.js
+++ source/js/menu.js
@@ -1,6 +1,6 @@
-var menu = document.querySelector('.main-nav__list');
-var openMenu = document.querySelector('.toggle');
-var openButton = document.querySelector('.toggle');
+let menu = document.querySelector('.main-nav__list');
+let openMenu = document.querySelector('.toggle');
+let openButton = document.querySelector('.toggle');
 
 openButton.classList.add('toggle--close');
 menu.classList.remove('main-nav__list--open');
@@ -11,36 +11,58 @@ openMenu.addEventListener('click', function () {
   openButton.classList.toggle('toggle--open');
 });
 
-var buttonTour = document.querySelectorAll('.button-buy');
-var formBuy = document.querySelector('.buy');
-var closeButton = document.querySelector('.close-button');
-var inputTel = document.querySelector('.form-buy__input--tel');
-var inputMail = document.querySelector('.form-buy__input--email');
+// let buttonTour = document.querySelectorAll('.button-buy');
+// let formBuy = document.querySelector('.buy');
+// let closeButton = document.querySelector('.close-button');
+// let inputTel = document.querySelector('.form-buy__input--tel');
+// let inputMail = document.querySelector('.form-buy__input--email');
 
-for(var i = 0; i < buttonTour.length; i++) {
-buttonTour[i].addEventListener('click', function () {
-  formBuy.classList.add('buy--opened');
-  inputTel.focus();
-});
-}
+// for(let i = 0; i < buttonTour.length; i++) {
+// buttonTour[i].addEventListener('click', function () {
+//   formBuy.classList.add('buy--opened');
+//   inputTel.focus();
+// });
+// }
 
-closeButton.addEventListener('click', function () {
-  formBuy.classList.remove('buy--opened');
-});
+// closeButton.addEventListener('click', function () {
+//   formBuy.classList.remove('buy--opened');
+// });
 
-formBuy.addEventListener("submit", function (evt) {
-  if (!inputTel.value || !inputMail.value) {
-    evt.preventDefault();
-  } else {
-    localStorage.setItem("tel", inputTel.value);
-    localStorage.setItem("email", inputMail.value);
-  }
-});
+// formBuy.addEventListener("submit", function (evt) {
+//   if (!inputTel.value || !inputMail.value) {
+//     evt.preventDefault();
+//   } else {
+//     localStorage.setItem("tel", inputTel.value);
+//     localStorage.setItem("email", inputMail.value);
+//   }
+// });
 
-window.addEventListener("keydown", function (evt) {
-  if (evt.key === 'Escape' || evt.key === 'Esc') {
-    if (formBuy.classList.contains("buy--opened")) {
-      formBuy.classList.remove("buy--opened");
-    }
-  }
-});
+// window.addEventListener("keydown", function (evt) {
+//   if (evt.key === 'Escape' || evt.key === 'Esc') {
+//     if (formBuy.classList.contains("buy--opened")) {
+//       formBuy.classList.remove("buy--opened");
+//     }
+//   }
+// });
+
+// let buyButton = document.querySelector('.form-buy__button');
+// let formSuccess = document.querySelector('.success');
+// let closeButtonSuccess = document.querySelector('.close-button--success');
+
+// buyButton.addEventListener('click', function (evt) {
+//   evt.preventDefault();
+//   formBuy.classList.remove("buy--opened");
+//   formSuccess.classList.add('success--opened');
+// });
+
+// closeButtonSuccess.addEventListener('click', function () {
+//   formSuccess.classList.remove('success--opened');
+// });
+
+// window.addEventListener("keydown", function (evt) {
+//   if (evt.key === 'Escape' || evt.key === 'Esc') {
+//     if (formSuccess.classList.contains("success--opened")) {
+//       formSuccess.classList.remove("success--opened");
+//     }
+//   }
+// });
